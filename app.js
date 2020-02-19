bb85fb02a88eb0c91a4e8a72aec8e9ebfdaf83f0
paroar <prodriguezarmida@gmail.com>
2020-02-19 09:33:12 +0100

    documentation

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var poll = require("./oscarsPoll.json");
 var app = express();
 var port = process.env.PORT || 3000;
 
-const swaggerOptions = {
+const options = {
   swaggerDefinition: {
     info: {
       title: "4Birds API",
@@ -16,29 +16,115 @@ const swaggerOptions = {
         name: "Amazing Developer"
       },
       servers: ["http://localhost:3000"]
+    },
+    "paths": {
+      "/getOscarsPoll": {
+        "get": {
+          "x-swagger-router-controller": "home",
+          "summary": "Get nominations",
+          "operationId": "index",
+          "tags": ["oscars"],
+          "description": "Get all the nominations for the Oscars gala 2020",
+          "parameters": [],
+          "responses": {}
+        }
+      },
+      "/getOscarsPoll/topic?topic={topic}": {
+        "get": {
+          "x-swagger-router-controller": "bar",
+          "summary": "Select nominations by topic",
+          "operationId": "impossible",
+          "tags": ["oscars"],
+          "description": "",
+          "parameters": [
+            {
+              name: "topic",
+              in: "path",
+              description: "Topic of the nomination",
+              required: true,
+              type: "string"
+            }
+          ],
+          "responses": {
+            "200": {
+              description: "successful operation",
+              schema: {
+                $ref: "#/definitions/Oscar"
+              }
+            },
+            "400": {
+              description: "Invalid username supplied"
+            },
+            "404": {
+              description: "User not found"
+            }
+          }
+        }
+      },
+      "/getOscarsPoll/nominations?nominations={nomination}": {
+        "get": {
+          "x-swagger-router-controller": "bar",
+          "summary": "Select nominations for film/actor",
+          "operationId": "impossible",
+          "tags": ["oscars"],
+          "description": "",
+          "parameters": [{
+            name: "nomination",
+            in: "path",
+            description: "Person or film to return",
+            required: true,
+            type: "string"
+          }],
+          "responses": {
+            "200": {
+              description: "successful operation",
+              schema: {
+                $ref: "#/definitions/Oscar"
+              }
+            },
+            "400": {
+              description: "Invalid username supplied"
+            },
+            "404": {
+              description: "User not found"
+            }
+          }
+        }
+      }
+    },
+    definitions: {
+      "Oscar": {
+        "type": "object",
+        "properties": {
+          "nomination": {
+            "type": "object",
+            "properties": {
+              "username": {
+                "type": "string"
+              },
+              "firstName": {
+                "type": "string"
+              },
+              "lastName": {
+                "type": "string"
+              }
+            }
+          }
+        }
+      }
     }
+
   },
   apis: ["app.js"]
 };
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
+const specs = swaggerJsDoc(options);
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Example app listening on port !`);
 });
-
-
-/**
- * @swagger
- * /getOscarsPoll:
- * get:
- *  description: Use to request all customers
- *  responses:
- *    "200":
- *      description: A successful response
- */
 
 app.get("/getOscarsPoll", (req, res) => {
   res.status(200).json(poll);
